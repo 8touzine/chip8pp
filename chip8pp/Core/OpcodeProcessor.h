a31a756741e87b82145746517ecc635b2a1fb53a
8touzine <sabbou1070@gmail.com>
2025-09-29 02:32:09 +0200

    adding some opcode headers

--- chip8pp/Core/OpcodeProcessor.h
+++ chip8pp/Core/OpcodeProcessor.h
@@ -5,5 +5,8 @@ class OpcodeProcessor {
 public:
 	void opFF65(unsigned char opcode);
 	void opFF55(unsigned char opcode);
-
+	unsigned char opD000(unsigned char opcode);
+	void opFF33(unsigned char opcode);
+	void opFF29(unsigned char opcode);
+	void opFF1E(unsigned char opcode);
 };
